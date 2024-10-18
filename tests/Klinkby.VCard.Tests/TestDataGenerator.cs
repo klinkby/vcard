b60f4da1cd567deeeb03460bc8ba510c92b22f7b
Mads Klinkby <m@kli.dk>
2024-10-18 22:37:20 +0200

    test: add unit tests

---
+++ tests/Klinkby.VCard.Tests/TestDataGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+
+namespace Klinkby.VCard.Tests;
+
+public sealed class TestDataGenerator : IEnumerable<object[]>
+{
+    private const string ExpectedVAlarm =
+        "BEGIN:VALARM\nTRIGGER:trigger\nACTION:action\nDESCRIPTION:alarm\nEND:VALARM\n";
+
+    private const string ExpectedVEvent =
+        $"BEGIN:VEVENT\nORGANIZER:organizer\nDTSTART:20220101T000000Z\nDTEND:20220101T010000Z\nLOCATION:location\nDESCRIPTION:description\nTRANSP:transp\nSEQUENCE:1\nUID:uid\nDTSTAMP:20220101T000000Z\nSUMMARY:summary\nPRIORITY:1\nCLASS:class\n{ExpectedVAlarm}END:VEVENT\n";
+
+    private const string ExpectedVCalendar =
+        $"BEGIN:VCALENDAR\nMETHOD:PUBLISH\n{ExpectedVEvent}{ExpectedVEvent}END:VCALENDAR\n";
+
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return [ExpectedVAlarm, CreateVAlarm()];
+        yield return [ExpectedVEvent, CreateVEvent()];
+        yield return [ExpectedVCalendar, CreateVCalendar()];
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private static VAlarm CreateVAlarm() =>
+        new()
+        {
+            Trigger = "trigger",
+            Action = "action",
+            Description = "alarm"
+        };
+
+    private static VEvent CreateVEvent() =>
+        new()
+        {
+            Organizer = "organizer",
+            DtStart = new DateTime(2022, 1, 1, 0, 0, 0, DateTimeKind.Utc),
+            DtEnd = new DateTime(2022, 1, 1, 1, 0, 0, DateTimeKind.Utc),
+            Location = "location",
+            Description = "description",
+            Transp = "transp",
+            Sequence = 1,
+            UId = "uid",
+            DtStamp = new DateTime(2022, 1, 1, 0, 0, 0, DateTimeKind.Utc),
+            Summary = "summary",
+            Priority = 1,
+            Class = "class",
+            Alarm = CreateVAlarm()
+        };
+
+    private static VCalendar CreateVCalendar() =>
+        new()
+        {
+            Method = "PUBLISH",
+            Events =
+            [
+                CreateVEvent(),
+                CreateVEvent()
+            ]
+        };
+}
---
+++ tests/Klinkby.VCard.Tests/VCardTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace Klinkby.VCard.Tests;
+
+[Trait("Category", "Unit")]
+public sealed class VCardTest
+{
+    [Theory]
+    [ClassData(typeof(TestDataGenerator))]
+    
+    public void WriteVCard(string expected, IVCardWriter testData)
+    {
+        using var sw = new StringWriter();
+        Debug.Assert(testData != null, nameof(testData) + " != null");
+        testData.WriteVCard(sw);
+        var actual = sw.ToString();
+
+        Assert.Equal(expected, actual);
+    }
+}
